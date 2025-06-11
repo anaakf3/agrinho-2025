3b7ae154b200f5f723f24ee83ca3e9fbfe8d163e
Ana Krebs <ana.krebs@escola.pr.gov.br>
2025-06-11 10:33:44 -0300

    5

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 document.querySelector('.menu').addEventListener('click', () => {
   alert("Menu ainda não implementado 🌿");
 });
+const menuBotao = document.querySelector('.menu');
+const menuLateral = document.getElementById('menuLateral');
+const fecharMenu = document.getElementById('fecharMenu');
+
+// Abrir menu
+menuBotao.addEventListener('click', () => {
+  menuLateral.classList.add('aberto');
+});
+
+// Fechar menu
+fecharMenu.addEventListener('click', () => {
+  menuLateral.classList.remove('aberto');
+});
